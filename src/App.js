ff6156f3beb8ed3159120910f876e7b4631aeb52
ArshadMuthalif <arshad@mayorainfotech.com>
2020-12-06 10:48:41 +0530

    dropdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react'
+import { DropDown } from './components/DropDown'
 
 
 
@@ -15,7 +16,7 @@ function App() {
 
   return (
     <div >
-
+      <DropDown />
     </div>
   );
 }
---
+++ src/components/DropDown.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const DropDown = () => {
+    return (
+        <div>
+
+            <label for="moreaction">More Action:</label>
+            <select name="moreaction" id="moreaction">
+
+                <option value="view">View</option>
+                <option value="edit">Edit</option>
+
+                <option value="send">Send</option>
+                <option value="delete" class="delete">Delete</option>
+
+            </select>
+        </div>)
+}
